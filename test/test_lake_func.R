5751dd16d96abd5129ade827add27a8bf6e5c76c
Alia Ajina <aliaajina@gmail.com>
2023-03-15 10:34:54 -0700

    making sure our test works

--- test/test_lake_func.R
+++ test/test_lake_func.R
@@ -1,4 +1,9 @@
 # Testing that our lake_func works with two different sets of data
+library(here)
+library(testthat)
+
+source(here("R","lake_func.R"))
+
 test_that("lake_func_works", {
   expect_equal(lake_func(initial_level = 100000, precip_data = 10, outflow = 10, evap = 1), list(99999,-1))
   expect_equal(lake_func(initial_level = 200, precip_data = 1, outflow = 1, evap = 1), list(199,-1))
