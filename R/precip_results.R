6fb28eb6a8ebd76e5a241fc64d91e066994a2167
SophiaLeiker <sophia.leiker@gmail.com>
2023-03-15 10:30:25 -0700

    adding precip resultsd ocumentation

---
+++ R/precip_results.R
@@ -0,0 +1,14 @@
+#' Lake Data
+#'
+#' @description
+#' This dataset is a representation of various precipitation values
+#'
+#' @format A data frame with 1 column and 4 rows:
+#' \describe{
+#'   \item{precip_results}{numeric vector}
+#' }
+#'
+#' @source Random Dataset created by using rnorm(mean = 10, sd = 2, n=nsample)
+#'
+
+"(data here)"
