15b94244d0f63f9fbb2c3577ba6dd946e7da665b
Alia Ajina <aliaajina@gmail.com>
2023-03-15 14:20:07 -0700

    Updating info in function

--- R/lake_func.R
+++ R/lake_func.R
@@ -1,5 +1,6 @@
 #' Lake volume function
-#'
+#' This function computes the volume of 4 lakes based on four different precipitation values in the provided dataset.
+#' You can use this equation for any lake by providing it with the initial lake level, precipitation data, outflow, and evaportion rates.
 #' @author Alia Ajina & Sophia Leiker
 #' @param intitial_level, changing initital lake level (gallons)
 #' @param precip_data, changing daily precipitation data (gallons)
